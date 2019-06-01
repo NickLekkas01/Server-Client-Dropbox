7338839b6d781f6cc2d7ed27102d314bc2b5e602
Qwerkykk <thebeaterkk@hotmail.com>
2019-06-01 23:40:00 +0300

    GET_FILE_LIST

--- dropbox_client.c
+++ dropbox_client.c
@@ -30,6 +30,7 @@ int sock;
 
 int POOL_SIZE;
 int PORT;
+char dirName[40];
 
 Client_data *start = NULL;
 
@@ -123,19 +124,55 @@ void fill_pool()
   }
 }
 
+void ask_file_list(int sock,struct in_addr* address,uint32_t port){
+  char message[256];
+  char path[128];
+  int bytes;
+  int files;
+  time_t version;
+  strcpy(message,"GET_FILE_LIST ");
+  write(sock,message,15);
+
+  while((bytes = read(sock,message,11 + sizeof(int)))<= 0){}
+  memcpy(&files,message+11,sizeof(int) );
+  printf("%s: %d\n",message,files);
+
+  while(files > 0 ){
+    
+    while(read(sock,message,128 +sizeof(uint64_t))<= 0){}
+    
+    memcpy(&version,message+128,sizeof(uint64_t));
+    place(&pool,address->s_addr,port,version,message);
+    pthread_cond_signal(&cond_nonempty);
+    }
+    files--;
+  }
+}
+
+
 void* worker_Thread(void* ptr) 
 {
   Pool_data *data;
   Client_data *temp = start;
   struct in_addr a;
+  int sock;
   char ip[50];
+
   while (1) 
   {
     data = obtain(&pool);
+    pthread_cond_signal(&cond_nonfull);
     a.s_addr = data->IP;
     strcpy(ip, inet_ntoa(a));  
-    printf("consumer: %s %d\n", ip, data->port);
-    pthread_cond_signal(&cond_nonfull);
+
+    if(data->path[0] == '\0'){
+    printf("Asking File List From Client: %s %d\n", ip, data->port);
+    //FIX prepei na ginei to connection me ton client edo kai to socket pou tha epistrafei na dothei stin sinartisi apo kato
+
+    ask_file_list(sock,&a,data->port);
+
+    }
+
     usleep(500000);
   }
 }
@@ -359,7 +396,7 @@ int read_from_server(int filedes, uint32_t myIP)
 
 int read_from_client (int filedes, struct sockaddr_in clientname, int sock)
 {
-    char buffer[MAXMSG];
+    char buffer[256];
     strcpy(buffer, "");
     int nbytes;
     char c[2];
@@ -388,10 +425,50 @@ int read_from_client (int filedes, struct sockaddr_in clientname, int sock)
     if(strcmp(buffer, "GET_FILE_LIST") == 0)
     {
         int counter;
+        char message[256];
+        
+        counter = file_count(dirName);
 
-        counter =
+        sprintf(message,"FILE_LIST ");
+        memcpy(message + 11,&counter,sizeof(int));
+        write(filedes,message,11 + sizeof(int));
+    
 
     }
+
+}
+
+void send_file_paths(int filedes, char* path){
+  char filePath[128];
+  char message[256];
+  struct dirent* directory;
+  DIR* dirptr;
+  struct stat stats;
+  
+
+  dirptr = opendir(path);
+
+  while((directory = readdir(dirptr))!= NULL){
+    if(directory->d_name[0] == '.' && directory->d_name == '.')
+      continue;
+
+    sprintf(filePath,"%s/%s",path,directory->d_name);
+    
+    if(opendir(filePath) != NULL){
+      send_file_paths(filedes,path);
+      continue;
+    }
+
+    stat(filePath,&stats);
+
+    memcpy(message,filePath,128);
+    memcpy(message + 128,&stats.st_mtime,sizeof(uint64_t));
+
+    write(filedes,message,128+sizeof(uint64_t));
+
+  }
+
+  close(dirptr);
 
 }
 
@@ -406,11 +483,16 @@ int file_count(char* path){
       if ( directory->d_name[0] == '.' && directory->d_name[1] == '.')
         continue;
 
-        strcpy(filePath,path,strlen(path));
-        strcpy(filePath + strlen(path),"/", 1 );
-        strcpy(filePath + strelen(path) + 1 , directory->d_name, strlen(directory->d_name) + 1);
+        memcpy(filePath,path,strlen(path));
+        memcpy(filePath + strlen(path),"/", 1 );
+        memcpy(filePath + strelen(path) + 1 , directory->d_name, strlen(directory->d_name) + 1);
 
+        if(opendir(filePath)!=NULL){
+          counter += file_count(filePath);
+          continue;
+        }
 
+        counter++;
     }
 
     closedir(dirptr);
@@ -437,6 +519,7 @@ void init_sockaddr (struct sockaddr_in *name,
   name->sin_addr = *(struct in_addr *) hostinfo->h_addr;
 }
 
+
 int main (int argc, char *argv[])
 {
   extern void init_sockaddr (struct sockaddr_in *name,
@@ -445,7 +528,7 @@ int main (int argc, char *argv[])
   struct sockaddr_in servername;
   fd_set active_fd_set, read_fd_set;
   size_t size;
-  char dirName[40];
+  
   int workerThreads;
   char SERVERHOST[50];
   if(argv[1][1] == 'd')
